df182de4d15878ce1ab8abb6b90c1a475c8c9d7e
Wiep van der Toorn <wiep.vandertoorn@gmail.com>
2023-04-18 17:00:28 +0200

    fix cython compile warnings

--- setup.py
+++ setup.py
@@ -1,15 +1,26 @@
 import numpy as np
 from Cython.Build import cythonize
-from Cython.Distutils import build_ext
 from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
 
 VERSION = "0.0.1"
+
+
+
+# Avoid a gcc warning below:
+# cc1plus: warning: command line option ‘-Wstrict-prototypes’ is valid for C/ObjC but not for C++
+class BuildExt(build_ext):
+    def build_extensions(self):
+        if '-Wstrict-prototypes' in self.compiler.compiler_so:
+            self.compiler.compiler_so.remove('-Wstrict-prototypes')
+        super().build_extensions()
     
 ext_modules = [
     Extension(name=str("adapt._c_llr_segmentation"),
               sources=[str("adapt/_c_llr_segmentation.pyx")],
               include_dirs=[np.get_include()],
-              language="c++")
+              language="c++",
+              define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')],)
 ]
 
 for e in ext_modules:
@@ -33,8 +44,8 @@ setup(
         ]
     },
     include_package_data=True,
-    ext_modules=cythonize(ext_modules),
-    cmdclass={'build_ext': build_ext},
+    ext_modules=cythonize(ext_modules, language_level = "3"),
+    cmdclass={'build_ext': BuildExt},
     test_suite='pytest',
     tests_require=['pytest'],
     classifiers=[
